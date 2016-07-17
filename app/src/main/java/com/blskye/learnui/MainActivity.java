fca216e0fa6d0817f323c64b69a707648a1257ea
wanghaotian <echowht@qq.com>
2016-07-17 14:41:53 +0800

    sqlite CRUD操作

--- app/src/main/java/com/blskye/learnui/MainActivity.java
+++ app/src/main/java/com/blskye/learnui/MainActivity.java
@@ -1,10 +1,14 @@
 package com.blskye.learnui;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -18,6 +22,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.blskye.learnui.adapter.FruitAdapter;
+import com.blskye.learnui.db.MyDatabaseHelper;
 import com.blskye.learnui.entity.Fruit;
 
 import java.util.ArrayList;
@@ -31,6 +36,20 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private ProgressBar progressBar;
     private Button button3;
     private Button button4;
+
+    private Button button5;
+    private MyDatabaseHelper myDatabaseHelper;
+
+    private Button button6;
+
+    private Button button7;
+
+    private Button button8;
+
+    private Button button9;
+
+    private Button button10;
+
 
     private String[] data={"Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango","dsad"};
 
@@ -57,6 +76,26 @@ public class MainActivity extends Activity implements View.OnClickListener{
         button4= (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
+
+        myDatabaseHelper=new MyDatabaseHelper(this,"BookStore.db",null,2);
+        button5= (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+
+        button6= (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        button7= (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        button8= (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+
+        button9= (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+
+        button10= (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(this);
         initFruits();
         FruitAdapter fruitAdapter=new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
 
@@ -115,6 +154,87 @@ public class MainActivity extends Activity implements View.OnClickListener{
                 });
                 dialog.show();
                 break;
+            case R.id.button5:
+                myDatabaseHelper.getWritableDatabase();
+                break;
+            case R.id.button6:
+                SQLiteDatabase db=myDatabaseHelper.getWritableDatabase();
+                ContentValues contentValues=new ContentValues();
+                contentValues.put("name", "The Da Vinci Code");
+                contentValues.put("author", "Dan Brown");
+                contentValues.put("pages", 454);
+                contentValues.put("price", 16.96);
+                //插入数据
+                db.insert("Book", null, contentValues);
+                contentValues.clear();
+
+                contentValues.put("author", "Dan Brown");
+                contentValues.put("pages", 510);
+                contentValues.put("price", 19.95);
+                db.insert("Book", null, contentValues);
+                contentValues.clear();
+
+                contentValues.put("name", "Hello");
+                contentValues.put("author", "wang");
+                contentValues.put("pages", 454);
+                contentValues.put("price", 16.96);
+                //插入数据
+                db.insert("Book", null, contentValues);
+                Toast.makeText(this,"Insert ",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button7:
+                SQLiteDatabase db2=myDatabaseHelper.getWritableDatabase();
+                ContentValues contentValues2=new ContentValues();
+                contentValues2.put("price",19.00);
+                db2.update("Book",contentValues2,"name=?",new String[]{"The Da Vinci Code"});
+                break;
+            case R.id.button8:
+                SQLiteDatabase db3=myDatabaseHelper.getWritableDatabase();
+                db3.delete("Book","name=?",new String[]{"Hello"});
+                Toast.makeText(this,"Delete ",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button9:
+                SQLiteDatabase db4=myDatabaseHelper.getWritableDatabase();
+                Cursor cursor=db4.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()){
+                    do {
+                        String name=cursor.getString(cursor.getColumnIndex("name"));
+                        String author=cursor.getString(cursor.getColumnIndex("author"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("main","Book:"+name+author+pages+price);
+
+
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
+                break;
+            case R.id.button10:
+                SQLiteDatabase db5=myDatabaseHelper.getWritableDatabase();
+//                开启事务
+                db5.beginTransaction();
+                try {
+                    db5.delete("Book",null,null);//清空Book表
+//                    if (true){
+//                        // 在这里手动抛出一个异常，让事务失败
+//                        throw new NullPointerException();
+//                    }
+                    ContentValues contentValues1=new ContentValues();
+                    contentValues1.put("name","Hello World");
+                    contentValues1.put("author", "Wanghaotian");
+                    contentValues1.put("pages", 720);
+                    contentValues1.put("price", 20.85);
+                    db5.insert("Book", null, contentValues1);
+                    db5.setTransactionSuccessful();
+                    Toast.makeText(this,"OK",Toast.LENGTH_LONG).show();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    db5.endTransaction();
+                }
+
             default:
                 break;
         }
